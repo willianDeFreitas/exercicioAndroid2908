a3bd930d6e604bf360500a5ceec34f7eb25778f4
willianDeFreitas <willian.freitasoliveira@yahoo.com.br>
2019-08-29 21:18:56 -0300

    correcao conexao com retrofit

--- app/src/main/java/com/example/aula29_08_2019/RetrofitService.java
+++ app/src/main/java/com/example/aula29_08_2019/RetrofitService.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
-    private String baseUrl = "http://10.10.90.100/user";
+    private String baseUrl = "http://10.10.90.100:8080/";
     private ApiEndPoint api;
     private static RetrofitService instancia;
 
